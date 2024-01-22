14bfbd9b94da8471194f4ba8c505a3241faed4d9
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 14:31:46 +0800

    Added an input function so that the user can input a word for the program.

---
+++ letters_in_even_numbers.py
@@ -0,0 +1,17 @@
+# Create a program that will --
+
+# Accept a string from the user and display characters that are present at an even index number.
+
+# DEADLINE: JANUARY 26, 2024
+
+# Expected results
+# Orginal String is pynative
+# Printing only even index chars
+# p
+# n
+# t
+# v
+
+# pseudocode
+word_given = str(input("Enter your word: "))
+print(word_given)
